f3372f89342135f9522d72d1a23485fda74778f6
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-10-21 17:55:08 -0300

    feat(session): create login methods

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,7 +1,9 @@
 const routes = require("express").Router();
 
 const userRoutes = require("./user");
+const sessionRoutes = require("./session");
 
 routes.use("/users", userRoutes);
+routes.use("/", sessionRoutes);
 
 module.exports = routes;
